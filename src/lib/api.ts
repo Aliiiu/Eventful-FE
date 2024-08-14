852393550f6947b66f851f385a7f5edd0fd3756c
Aliu Salaudeen <ayokunumi99@gmail.com>
2024-08-15 00:05:15 +0100

    updating create user api

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -51,3 +51,17 @@ export async function buyTicket(eventId: string): Promise<TEvent> {
 		body: JSON.stringify({ eventId }),
 	});
 }
+
+export const registerCreator = async (creatorData: any) => {
+	return fetchAPI(`register/creator`, {
+		method: 'POST',
+		body: creatorData,
+	});
+};
+
+export const registerUser = async (userData: any) => {
+	return fetchAPI(`register/attendee`, {
+		method: 'POST',
+		body: userData,
+	});
+};
